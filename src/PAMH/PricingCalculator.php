809ab9cd65751f62b3017ab45c6670c716887458
matiit <matiqing@gmail.com>
2014-07-11 17:30:00 +0200

    inject PriceHolder into calculator

--- src/PAMH/PricingCalculator.php
+++ src/PAMH/PricingCalculator.php
@@ -4,6 +4,18 @@ namespace PAMH;
 
 class PricingCalculator implements PricingCalculatorInterface
 {
+	/**
+	 * PriceHolder object
+	 *
+	 * @var PriceHolder
+	 */
+	protected $priceHolder;
+
+	public function __construct(PriceHolder $priceHolder)
+	{
+		$this->priceHolder = $priceHolder;
+	}
+
     /**
      * Calculate a price based upon an array of start and
      * end date pairs.
